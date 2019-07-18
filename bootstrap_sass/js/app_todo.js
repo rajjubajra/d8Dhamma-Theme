343ccd1cb955d0964c872a974c184ffab1fc5de1
rajju <vajrachara@yahoo.com>
2019-07-18 21:27:29 +0100

    todo-note show hide script remaining code to front-page.html.twig

--- bootstrap_sass/js/app_todo.js
+++ bootstrap_sass/js/app_todo.js
@@ -1,51 +1,5 @@
 // CODE EXPLAINED channel
-// Select the Elements
-const clear = document.querySelector(".clear");
-const dateElement = document.getElementById("date");
-const list = document.getElementById("list");
-const input = document.getElementById("input");
-const todoNoteBtn = document.getElementById("todo-note-btn");
 
-// Classes names
-const CHECK = "fa-check-circle";
-const UNCHECK = "fa-circle";
-const LINE_THROUGH = "lineThrough";
-
-// Variables
-let LIST, id;
-
-// get item from localstorage
-let data = localStorage.getItem("TODO");
-
-// check if data is not empty
-if(data){
-    LIST = JSON.parse(data);
-    id = LIST.length; // set the id to the last one in the list
-    loadList(LIST); // load the list to the user interface
-}else{
-    // if data isn't empty
-    LIST = [];
-    id = 0;
-}
-
-// load items to the user's interface
-function loadList(array){
-    array.forEach(function(item){
-        addToDo(item.name, item.id, item.done, item.trash);
-    });
-}
-
-// clear the local storage
-clear.addEventListener("click", function(){
-    localStorage.clear();
-    location.reload();
-});
-
-// Show todays date
-const options = {weekday : "long", month:"short", day:"numeric"};
-const today = new Date();
-
-dateElement.innerHTML = today.toLocaleDateString("en-US", options);
 
 
 
